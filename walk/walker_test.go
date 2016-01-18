223ad5650b1d0750399728734fd3e402fec114ac
wolfgarnet <wolfgarnet@gmail.com>
2016-01-18 09:13:04 +0100

    Fixed walker unit test

--- walk/walker_test.go
+++ walk/walker_test.go
@@ -45,7 +45,7 @@ func TestWalker(t *testing.T) {
 			t.Errorf("[%v] Failed, number of ancestors not correct, %v != %v", i, test.size, len(visitor.ancestors))
 		}
 
-		parent := visitor.ancestors[len(visitor.ancestors)-2].Parent
+		parent := visitor.ancestors[len(visitor.ancestors)-2].Parent()
 		typeOfParent := reflect.TypeOf(parent)
 
 		if test.parent != typeOfParent {
