8cf0e1942a91502956d2927de9527f94ccae9c55
Steven Hartland <steven.hartland@multiplay.co.uk>
2022-11-28 23:56:52 +0000

    feat(underscore)!: update to v1.13.6
    Update underscore.js to v1.13.6.
    
    BREAKING CHANGE: This changes the behaviour for the following underscore
    functions, please see underscore.js documentation for details.
    * .template
    * .keys
    * .after
    * .range
    * .reduce
    * .reduceRight
    * .reduceLeft
    
    Fixes #110

--- underscore/underscore.go
+++ underscore/underscore.go
@@ -1,33 +1,35 @@
-/*
-Package underscore contains the source for the JavaScript utility-belt library.
-
-	import (
-		_ "github.com/robertkrimen/otto/underscore"
-	)
-	// Every Otto runtime will now include underscore
-
-http://underscorejs.org
-
-https://github.com/documentcloud/underscore
-
-By importing this package, you'll automatically load underscore every time you create a new Otto runtime.
-
-To prevent this behavior, you can do the following:
-
-	import (
-		"github.com/robertkrimen/otto/underscore"
-	)
-
-	func init() {
-		underscore.Disable()
-	}
-*/
+// Package underscore contains the source for the JavaScript utility-belt library.
+//
+//	import (
+//		_ "github.com/robertkrimen/otto/underscore"
+//	)
+//
+// Every Otto runtime will now include [underscore] for more information see the [underscore docs]
+//
+// By importing this package, you'll automatically load underscore every time you create a new Otto runtime.
+//
+// To prevent this behavior, you can do the following:
+//
+//	import (
+//		"github.com/robertkrimen/otto/underscore"
+//	)
+//
+//	func init() {
+//		underscore.Disable()
+//	}
+//
+// [underscore]: http://underscorejs.org
+// [underscore docs]: https://github.com/documentcloud/underscore
 package underscore
 
 import (
+	_ "embed"
+
 	"github.com/robertkrimen/otto/registry"
 )
 
+//go:embed underscore-min.js
+var underscore string
 var entry *registry.Entry = registry.Register(Source)
 
 // Enable underscore runtime inclusion.
@@ -42,5 +44,5 @@ func Disable() {
 
 // Source returns the underscore source.
 func Source() string {
-	return string(underscore())
+	return underscore
 }
